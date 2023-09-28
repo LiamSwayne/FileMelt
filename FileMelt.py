62d6f194e319a260a81b3210804a0168ae32a5c1
Liam Swayne <liamtswayne@gmail.com>
2023-09-28 14:15:28 -0400

    Update FileMelt.py

--- FileMelt.py
+++ FileMelt.py
@@ -41,4 +41,4 @@ for root, _, files in os.walk(inputFolder):
 print("Total bytes in input folder: " + str(totalInputBytes) + " bytes")
 print("Total bytes in output folder: " + str(totalOutputBytes) + " bytes")
 percentDecrease = 1 - (totalOutputBytes / float(totalInputBytes))
-print("Percent decrease: " + str(round(percentDecrease*100, 4)) + "%")
+print("Percent decrease: " + str(round(percentDecrease * 100, 4)) + "%")
