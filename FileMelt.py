1447873d5c9b01ab51fd08254c911cf43788d1c2
Liam Swayne <liamtswayne@gmail.com>
2023-09-28 11:03:09 -0400

    use camelcase

--- FileMelt.py
+++ FileMelt.py
@@ -2,57 +2,57 @@ import os
 import shutil
 
 # Define input and output folders
-input_folder = "source"
-output_folder = "docs"
+inputFolder = "source"
+outputFolder = "docs"
 
-def minify_html(input_path, output_path):
+def minifyHtml(inputPath, outputPath):
     # Create the output directory if it doesn't exist
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
+    if not os.path.exists(outputPath):
+        os.makedirs(outputPath)
 
-    total_input_bytes = 0
-    total_output_bytes = 0
+    totalInputBytes = 0
+    totalOutputBytes = 0
 
-    for root, _, files in os.walk(input_path):
+    for root, _, files in os.walk(inputPath):
         for filename in files:
-            input_file = os.path.join(root, filename)
-            output_file = os.path.join(output_path, filename)
+            inputFile = os.path.join(root, filename)
+            outputFile = os.path.join(outputPath, filename)
 
             if filename.endswith(".html"):
-                with open(input_file, "r") as infile, open(output_file, "w") as outfile:
-                    html_content = infile.read()
-                    cleaned_html = remove_html_comments(html_content)
-                    outfile.write(cleaned_html)
+                with open(inputFile, "r") as infile, open(outputFile, "w") as outfile:
+                    htmlContent = infile.read()
+                    cleanedHtml = removeHtmlComments(htmlContent)
+                    outfile.write(cleanedHtml)
             else:
                 # Copy non-HTML files to the output folder
-                shutil.copy(input_file, output_file)
+                shutil.copy(inputFile, outputFile)
 
             # Calculate total bytes
-            total_input_bytes += os.path.getsize(input_file)
-            total_output_bytes += os.path.getsize(output_file)
+            totalInputBytes += os.path.getsize(inputFile)
+            totalOutputBytes += os.path.getsize(outputFile)
 
-    return total_input_bytes, total_output_bytes
+    return totalInputBytes, totalOutputBytes
 
-def remove_html_comments(html):
+def removeHtmlComments(html):
     result = ""
-    inside_comment = False
+    insideComment = False
     i = 0
     while i < len(html):
         if html[i:i+4] == "<!--":
-            inside_comment = True
+            insideComment = True
             i += 4
         elif html[i:i+3] == "-->":
-            inside_comment = False
+            insideComment = False
             i += 3
-        elif not inside_comment:
+        elif not insideComment:
             result += html[i]
             i += 1
         else:
             i += 1
     return result
 
-total_input_bytes, total_output_bytes = minify_html(input_folder, output_folder)
-print("Total bytes in input folder: " + str(total_input_bytes) + " bytes")
-print("Total bytes in output folder: " + str(total_output_bytes) + " bytes")
-percent_decrease = 1 - (total_output_bytes / float(total_input_bytes))
-print("Percent decrease: " + str(round(percent_decrease*100,4)) + "%")
+totalInputBytes, totalOutputBytes = minifyHtml(inputFolder, outputFolder)
+print("Total bytes in input folder: " + str(totalInputBytes) + " bytes")
+print("Total bytes in output folder: " + str(totalOutputBytes) + " bytes")
+percentDecrease = 1 - (totalOutputBytes / float(totalInputBytes))
+print("Percent decrease: " + str(round(percentDecrease*100, 4)) + "%")
