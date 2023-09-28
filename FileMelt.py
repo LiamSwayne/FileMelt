975b1d4feaeb6c32c9538a7a2cd5394994fb47a2
Liam Swayne <liamtswayne@gmail.com>
2023-09-28 12:45:47 -0400

    switch to minifyhtml

--- FileMelt.py
+++ FileMelt.py
@@ -1,71 +1,43 @@
 import os
 import shutil
-import re
+from htmlmin import minify
 
-# SETTINGS
+### SETTINGS
 inputFolder = "source"
 outputFolder = "docs"
 
-def minifyHtml(inputPath, outputPath):
-    # Create the output directory if it doesn't exist
-    if not os.path.exists(outputPath):
-        os.makedirs(outputPath)
+### Methods
 
-    totalInputBytes = 0
-    totalOutputBytes = 0
+def minifyHtml(inputFile, outputFile):
+    with open(inputFile, "r") as infile, open(outputFile, "w") as outfile:
+        htmlContent = infile.read()
+        minifiedHtml = minify(htmlContent, remove_empty_space=True)
+        outfile.write(minifiedHtml)
 
-    for root, _, files in os.walk(inputPath):
-        for filename in files:
-            inputFile = os.path.join(root, filename)
-            outputFile = os.path.join(outputPath, filename)
+### Main program
 
-            if filename.endswith(".html"):
-                with open(inputFile, "r") as infile, open(outputFile, "w") as outfile:
-                    htmlContent = infile.read()
-                    cleanedHtml = removeHtmlComments(htmlContent)
-                    outfile.write(cleanedHtml)
-            else:
-                # Copy non-HTML files to the output folder
-                shutil.copy(inputFile, outputFile)
+# Create the output directory if it doesn't exist
+if not os.path.exists(outputFolder):
+    os.makedirs(outputFolder)
 
-            # Calculate total bytes
-            totalInputBytes += os.path.getsize(inputFile)
-            totalOutputBytes += os.path.getsize(outputFile)
+totalInputBytes = 0
+totalOutputBytes = 0
 
-    return totalInputBytes, totalOutputBytes
+for root, _, files in os.walk(inputFolder):
+    for filename in files:
+        inputFile = os.path.join(root, filename)
+        outputFile = os.path.join(outputFolder, filename)
 
-def removeHtmlComments(html):
-    result = ""
-    insideComment = False
-    i = 0
-
-    # Replace all strings with placeholders
-    placeholders = []
-    string_pattern = r'"(?:\\.|[^"\\])*"'
-    multiline_string_pattern = r'`[^`]*`'
-    html = re.sub(string_pattern, lambda x: placeholders.append(x.group()) or f"__STRING_PLACEHOLDER_{len(placeholders) - 1}__", html)
-    html = re.sub(multiline_string_pattern, lambda x: placeholders.append(x.group()) or f"__MULTILINE_STRING_PLACEHOLDER_{len(placeholders) - 1}__", html)
-
-    while i < len(html):
-        if html[i:i+4] == "<!--":
-            insideComment = True
-            i += 4
-            while i < len(html) and html[i:i+3] != "-->":
-                i += 1
-            if i < len(html):
-                i += 3
+        if filename.endswith(".html"):
+            minifyHtml(inputFile, outputFile)
         else:
-            result += html[i]
-            i += 1
+            # Copy non-HTML files to the output folder
+            shutil.copy(inputFile, outputFile)
 
-    # Restore the original strings
-    for index, placeholder in enumerate(placeholders):
-        result = result.replace(f"__STRING_PLACEHOLDER_{index}__", placeholder)
-        result = result.replace(f"__MULTILINE_STRING_PLACEHOLDER_{index}__", placeholder)
+        # Calculate total bytes
+        totalInputBytes += os.path.getsize(inputFile)
+        totalOutputBytes += os.path.getsize(outputFile)
 
-    return result
-
-totalInputBytes, totalOutputBytes = minifyHtml(inputFolder, outputFolder)
 print("Total bytes in input folder: " + str(totalInputBytes) + " bytes")
 print("Total bytes in output folder: " + str(totalOutputBytes) + " bytes")
 percentDecrease = 1 - (totalOutputBytes / float(totalInputBytes))
